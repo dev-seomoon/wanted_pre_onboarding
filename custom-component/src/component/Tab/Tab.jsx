835a3a68719e401c745e7581e913d069f69985ab
from97 <skm1104@daum.net>
2022-02-03 21:22:46 +0900

    [feat] Tab 구현

--- custom-component/src/component/Tab/Tab.jsx
+++ custom-component/src/component/Tab/Tab.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import TabStyled from "./Tab.styles";
 
-const Tab = () => <TabStyled>Tab</TabStyled>;
+const Tab = () => {
+  const tabs = ["Tab1", "Tab2", "Tab3"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  return (
+    <TabStyled>
+      <TabStyled.Nav>
+        <ul>
+          {tabs.map((tab, index) => (
+            <li key={tab} onClick={() => setCurrentIndex(index)} className={index === currentIndex ? "active" : ""}>
+              {tab}
+            </li>
+          ))}
+        </ul>
+      </TabStyled.Nav>
+      <TabStyled.Content>{tabs[currentIndex]}</TabStyled.Content>
+    </TabStyled>
+  );
+};
 
 export default Tab;
--- custom-component/src/component/Tab/Tab.styles.jsx
+++ custom-component/src/component/Tab/Tab.styles.jsx
@@ -1,5 +1,39 @@
 import styled from "styled-components";
 
-const TabStyled = styled.div``;
+const TabStyled = styled.div`
+  width: 80%;
+`;
+
+TabStyled.Nav = styled.nav`
+  ul {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    background: #eee;
+    height: 30px;
+  }
+
+  li {
+    width: 30%;
+    line-height: 30px;
+    padding-left: 10px;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+
+  li.active {
+    background: purple;
+    color: #fff;
+    transition: all 0.3s;
+  }
+`;
+
+TabStyled.Content = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default TabStyled;
