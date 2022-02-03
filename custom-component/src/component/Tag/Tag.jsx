1a5eaa8a1edd2d2a0f5d4dc3c13c1671d78c7206
from97 <skm1104@daum.net>
2022-02-03 21:59:42 +0900

    [feat] Tag 구현

--- custom-component/src/component/Tag/Tag.jsx
+++ custom-component/src/component/Tag/Tag.jsx
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import TagStyled from "./Tag.styles";
 
-const Tag = () => <TagStyled>Tag</TagStyled>;
+const Tag = () => {
+  const [value, setValue] = useState("");
+  const [tagList, setTagList] = useState(["tag 1", "tag 2"]);
+  const handleInputChange = e => {
+    setValue(e.target.value);
+  };
+  const addTag = e => {
+    if (e.key === "Enter") {
+      setTagList([...tagList, e.target.value]);
+      setValue("");
+    }
+  };
+  const removeTag = tag => {
+    setTagList(tagList.filter(item => item !== tag));
+  };
+  return (
+    <TagStyled>
+      <ul>
+        {tagList.map(tag => (
+          <li key={tag}>
+            {tag}
+            <button
+              type="button"
+              onClick={() => {
+                removeTag(tag);
+              }}
+            >
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
+      <input value={value} onChange={handleInputChange} onKeyPress={addTag} placeholder="Press enter to add tags" />
+    </TagStyled>
+  );
+};
 
 export default Tag;
--- custom-component/src/component/Tag/Tag.styles.jsx
+++ custom-component/src/component/Tag/Tag.styles.jsx
@@ -1,5 +1,47 @@
 import styled from "styled-components";
 
-const TagStyled = styled.div``;
+const TagStyled = styled.div`
+  width: 80%;
+  min-width: 400px;
+  height: 40px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+
+  ul {
+    display: flex;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    background: purple;
+    text-align: center;
+    line-height: 30px;
+    border-radius: 5px;
+    margin-right: 10px;
+    padding: 0 10px;
+
+    button {
+      width: 14px;
+      height: 14px;
+      font-size: 12px;
+      text-align: center;
+      color: purple;
+      background: #fff;
+      border-radius: 100%;
+      margin-left: 8px;
+      cursor: pointer;
+    }
+  }
+
+  input {
+    border: none;
+    outline: none;
+  }
+`;
 
 export default TagStyled;
