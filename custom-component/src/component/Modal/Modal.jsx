d993f6a13aa89e397a3a287f79951772b370dab3
from97 <skm1104@daum.net>
2022-02-03 20:50:34 +0900

    [feat] Modal 구현

--- custom-component/src/component/Modal/Modal.jsx
+++ custom-component/src/component/Modal/Modal.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ModalStyled from "./Modal.styles";
 
-const Modal = () => <ModalStyled>Modal</ModalStyled>;
+const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <ModalStyled>
+      <ModalStyled.Overlay isOpen={isOpen} />
+      <ModalStyled.Box isOpen={isOpen}>
+        Hello, Modal!
+        <ModalStyled.Box.CloseButton onClick={handleClose} />
+      </ModalStyled.Box>
+      <ModalStyled.Button onClick={handleOpen}>Open Modal</ModalStyled.Button>
+    </ModalStyled>
+  );
+};
 
 export default Modal;
--- custom-component/src/component/Modal/Modal.styles.jsx
+++ custom-component/src/component/Modal/Modal.styles.jsx
@@ -1,5 +1,70 @@
 import styled from "styled-components";
 
-const ModalStyled = styled.div``;
+const ModalStyled = styled.div`
+  width: 80%;
+  height: 300px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+ModalStyled.Overlay = styled.div`
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+`;
+
+ModalStyled.Box = styled.div`
+  display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: calc(50% - 100px);
+  left: calc(50% - 150px);
+  width: 300px;
+  height: 200px;
+  background: #fff;
+  border-radius: 20px;
+  z-index: 100;
+`;
+
+ModalStyled.Box.CloseButton = styled.div`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  position: absolute;
+  top: 13px;
+  right: 15px;
+  :before,
+  :after {
+    position: absolute;
+    left: 14px;
+    content: " ";
+    height: 30px;
+    width: 2px;
+    background-color: #777;
+  }
+  :before {
+    transform: rotate(45deg);
+  }
+  :after {
+    transform: rotate(-45deg);
+  }
+`;
+
+ModalStyled.Button = styled.button`
+  width: 130px;
+  height: 45px;
+  color: #fff;
+  background: purple;
+  border-radius: 30px;
+  cursor: pointer;
+`;
 
 export default ModalStyled;
