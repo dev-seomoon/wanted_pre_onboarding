9ce9232669e53477c037c9726cfa658cbfe35b80
from97 <skm1104@daum.net>
2022-02-04 15:31:03 +0900

    [feat] Auto Complete 구현

--- custom-component/src/component/AutoComplete/AutoComplete.jsx
+++ custom-component/src/component/AutoComplete/AutoComplete.jsx
@@ -1,6 +1,60 @@
-import React from "react";
-import AutoCompleteStyled from "./AutoComplete.styles";
+import React, { useState } from "react";
+import AutoCompleteStyled, { SearchBar, SuggestionField, ResetButton } from "./AutoComplete.styles";
 
-const AutoComplete = () => <AutoCompleteStyled>Auto Complete</AutoCompleteStyled>;
+const AutoComplete = () => {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [showSuggestion, setShowSuggestion] = useState(false);
+
+  const handleSuggestion = input => {
+    const filtered = suggestions.filter(keyword => keyword.toLowerCase().indexOf(input.toLowerCase()) > -1 && input !== "");
+    if (filtered.length > 0) {
+      setFilteredSuggestions(filtered);
+      setShowSuggestion(true);
+    } else {
+      setFilteredSuggestions([]);
+      setShowSuggestion(false);
+    }
+  };
+
+  const handleInputChange = e => {
+    setValue(e.target.value);
+    handleSuggestion(e.target.value);
+  };
+
+  const handleEnter = e => {
+    if (e.key === "Enter") {
+      setSuggestions([...suggestions, e.target.value]);
+      setValue("");
+      setShowSuggestion(false);
+    }
+  };
+
+  const selectSuggestion = e => {
+    setValue(e.target.innerText);
+    handleSuggestion(e.target.innerText);
+  };
+
+  const handleReset = () => {
+    setValue("");
+    setFilteredSuggestions([]);
+    setShowSuggestion(false);
+  };
+
+  return (
+    <AutoCompleteStyled>
+      <SearchBar value={value} onChange={handleInputChange} onKeyPress={handleEnter} showSuggestion={showSuggestion} />
+      <ResetButton onClick={handleReset} />
+      <SuggestionField show={showSuggestion}>
+        {filteredSuggestions.map(keyword => (
+          <li key={keyword} onClick={selectSuggestion}>
+            {keyword}
+          </li>
+        ))}
+      </SuggestionField>
+    </AutoCompleteStyled>
+  );
+};
 
 export default AutoComplete;
--- custom-component/src/component/AutoComplete/AutoComplete.styles.jsx
+++ custom-component/src/component/AutoComplete/AutoComplete.styles.jsx
@@ -1,5 +1,56 @@
 import styled from "styled-components";
 
-const AutoCompleteStyled = styled.div``;
+const AutoCompleteStyled = styled.div`
+  width: 80%;
+  position: relative;
+`;
+
+export const SearchBar = styled.input`
+  width: 100%;
+  height: 50px;
+  border: 1px solid #ccc;
+  border-radius: ${({ showSuggestion }) => (showSuggestion ? "15px 15px 0 0" : "15px")};
+  padding-left: 10px;
+  box-sizing: border-box;
+  font-size: 15px;
+  :focus {
+    box-shadow: ${({ showSuggestion }) => (showSuggestion ? "none" : "0 5px 5px #bbb")};
+    outline: none;
+  }
+`;
+
+export const ResetButton = styled.div`
+  position: absolute;
+  width: 15px;
+  height: 15px;
+  top: 18px;
+  right: 10px;
+  text-align: center;
+  cursor: pointer;
+  :after {
+    content: "×";
+    font-size: 20px;
+    line-height: 15px;
+    color: #313131;
+  }
+`;
+
+export const SuggestionField = styled.ul`
+  display: ${({ show }) => (show ? "block" : "none")};
+  width: 100%;
+  border: 1px solid #ccc;
+  border-top: none;
+  border-radius: 0 0 15px 15px;
+  box-shadow: 0 5px 5px #bbb;
+  padding: 10px;
+  box-sizing: border-box;
+
+  li {
+    line-height: 20px;
+  }
+  li:hover {
+    background: #eee;
+  }
+`;
 
 export default AutoCompleteStyled;
