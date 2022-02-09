32b1e7373f2df42d530d1f20aea373243529b00d
from97 <skm1104@daum.net>
2022-02-09 21:37:41 +0900

    [fix] Tag 중복 에러 해결

--- custom-component/src/component/Tag/Tag.jsx
+++ custom-component/src/component/Tag/Tag.jsx
@@ -3,14 +3,14 @@ import TagStyled, { TagList, DeleteTagButton, TagInput } from "./Tag.styles";
 import useTag from "../../hook/useTag";
 
 const Tag = () => {
-  const { tagInput, tagList, removeTag, addTag } = useTag(["Wanted"]);
+  const { tagInput, tagList, removeTag, addTag } = useTag();
   return (
     <TagStyled>
       <TagList>
         {tagList.map(tag => (
-          <TagList.Item key={tag}>
-            {tag}
-            <DeleteTagButton onClick={() => removeTag(tag)}>x</DeleteTagButton>
+          <TagList.Item key={tag.id}>
+            {tag.value}
+            <DeleteTagButton onClick={() => removeTag(tag.id)}>x</DeleteTagButton>
           </TagList.Item>
         ))}
       </TagList>
--- custom-component/src/hook/useTag.js
+++ custom-component/src/hook/useTag.js
@@ -1,19 +1,22 @@
 import { useState } from "react";
+import nextId from "react-id-generator";
 import useInput from "./useInput";
 
-const useTag = (initialValue) => {
+const useTag = () => {
   const tagInput = useInput();
-  const [tagList, setTagList] = useState(initialValue);
+  const [tagList, setTagList] = useState([{
+    id: nextId(), value: "Wanted"
+  }]);
 
   const addTag = e => {
     if (e.key === "Enter") {
-      setTagList([...tagList, tagInput.inputProps.value]);
+      setTagList([...tagList, {id: nextId(), value: tagInput.inputProps.value} ]);
       tagInput.setValue("");
     }
   }
 
-  const removeTag = tag => {
-    setTagList(tagList.filter(item => item !== tag));
+  const removeTag = removeId => {
+    setTagList(tagList.filter(item => item.id !== removeId));
   }
 
   return { tagInput, tagList, removeTag, addTag };
